fff9ebaec7a0996433d0c50d8fa358a0a8809819
Flor <florxgomez@gmail.com>
2019-10-30 18:53:24 -0300

    modified PostList componnet

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions";
 
 class PostList extends React.Component {
-    render() {
-        return <div>Post List</div>
-    }
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
+  renderList() {
+    return this.props.posts.map(post => {
+      return (
+        <div className="item" key={post.id}>
+          <i className="large middle aligned icon user" />
+          <div className="content">
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return <div className="ui relaxed divided list">{this.renderList()}</div>;
+  }
 }
 
-export default PostList;
+const mapStateToProps = state => ({
+  posts: state.posts,
+});
+
+export default connect(
+  mapStateToProps,
+  { fetchPosts }
+)(PostList);
