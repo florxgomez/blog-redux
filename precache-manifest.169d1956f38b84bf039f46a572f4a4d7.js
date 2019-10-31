8356fd66c3bc5766fde44819a33a460dc8409460
Flor <florxgomez@gmail.com>
2019-10-30 23:28:53 -0300

    Updates

---
+++ precache-manifest.169d1956f38b84bf039f46a572f4a4d7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bc1826bc65b958391bab7f696e7d3db",
+    "url": "/my-blog/index.html"
+  },
+  {
+    "revision": "7c0a11c473eb27665e9e",
+    "url": "/my-blog/static/js/2.4a90d37b.chunk.js"
+  },
+  {
+    "revision": "1de6dfb8d229f6c386f7",
+    "url": "/my-blog/static/js/main.d20f1443.chunk.js"
+  },
+  {
+    "revision": "f98406f409e6fdac9b98",
+    "url": "/my-blog/static/js/runtime-main.d88e94fb.js"
+  }
+]);
